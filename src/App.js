d33db6fcd5c9fa609b2e327cfbd10cfe34e949d8
Michelle <michelle.en4@gmail.com>
2019-04-22 14:24:21 -0400

    coded social cards and styles

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import baby from './imgs/grumpybaby.jpg';
+import marshmallows from './imgs/marshmallows.jpg';
+import pattern from './imgs/pattern.jpg';
+
+const list = [
+  {image: baby,
+  title: 'Ut sit quod aliquip rationibus.',
+  description: 'Pri ea wisi accumsan complectitur, qui ea omnis contentiones? Eros harum corpora nec no, vis.',
+  url: 'http://www.url.com',
+  },
+  {
+  image: marshmallows,
+  title: 'Ut sit quod aliquip rationibus.',
+  description: 'Pri ea wisi accumsan complectitur, qui ea omnis contentiones? Eros harum corpora nec no, vis.',
+  url: 'http://www.myfavoritewebsite.org',
+  },
+  {
+  image: pattern,
+  title: 'Ut sit quod aliquip rationibus.',
+  description: 'Pri ea wisi accumsan complectitur, qui ea omnis contentiones? Eros harum corpora nec no, vis.',
+  url: 'http://www.secondfavorite.com',
+  },
+]
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list,
+    }
+
+    this.findSite = this.findSite.bind(this);
+  }
+
+  findSite(url){
+    return url.match(/[a-z]+\.[a-z]+$/)[0] || '';
+  }
+
   render() {
+    var list = this.state.list;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      list.map(item => {
+        return (
+          <div className='card'>
+            <img src={item.image} 
+              className="preview"/>
+              <h3>{item.title}</h3>
+              <p>{item.description}</p>
+              <p className="url">
+                {this.findSite(item.url)}
+              </p>
+          </div>
+        )
+      })
     );
   }
 }
