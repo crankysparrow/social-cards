fe34b5644ce0c987d3c4b5d38b34ebfd097ebda6
Michelle <michelle.en4@gmail.com>
2019-04-24 12:26:16 -0400

    Updates

---
+++ precache-manifest.ad77cb108ce3af86949d43ea58cc07cd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8be79fb1edb8535f0b6d",
+    "url": "/social-cards/static/css/main.3be8a03a.chunk.css"
+  },
+  {
+    "revision": "8be79fb1edb8535f0b6d",
+    "url": "/social-cards/static/js/main.60f661c3.chunk.js"
+  },
+  {
+    "revision": "d655a32aeeac77256fea",
+    "url": "/social-cards/static/js/runtime~main.46a02cbe.js"
+  },
+  {
+    "revision": "f5fe11c915f92668079e",
+    "url": "/social-cards/static/js/2.f105d4e9.chunk.js"
+  },
+  {
+    "revision": "4a8374ae41c8f6dcf388395390a01ffc",
+    "url": "/social-cards/static/media/grumpybaby.4a8374ae.jpg"
+  },
+  {
+    "revision": "08b4b96b4b6553a5b29cca0615ff882d",
+    "url": "/social-cards/static/media/marshmallows.08b4b96b.jpg"
+  },
+  {
+    "revision": "c17a7c21519d30a4745acb0150600297",
+    "url": "/social-cards/static/media/pattern.c17a7c21.jpg"
+  },
+  {
+    "revision": "1c6d7a201312c14f72048295f48e00f8",
+    "url": "/social-cards/index.html"
+  }
+];
